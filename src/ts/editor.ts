9106cea56053905f7a03a40ee3556a7e3ec38068
Omar Alhammadi <sequenceko@gmail.com>
2019-03-12 16:13:35 +0400

    editor display and save

--- src/ts/editor.ts
+++ src/ts/editor.ts
@@ -1,18 +1,17 @@
 import Konva from 'konva';
 import $ from 'jquery';
-import 'bootstrap';
 
-interface Vec2 {
-    x : number;
-    y : number;
+
+class Vec2 {
+    constructor(public x: number, public y: number) {}
 }
 
 class ELabel {
-    label : any;
-    text : any;
-    tag : any;
+    label: any;
+    text: any;
+    tag: any;
 
-    constructor(pos:Vec2, txt:string, fillColor:string, private container : any) {
+    constructor(pos: Vec2, txt: string, fillColor: string) {
         this.text = new Konva.Text({
             text: txt,
             fontFamily: 'Calibri',
@@ -37,33 +36,151 @@ class ELabel {
             x: pos.x,
             y: pos.y,
             draggable: true
+        });
+
+
+        this.label.add(this.tag);
+        this.label.add(this.text);
+    }
+}
+
+const scale: Vec2 = new Vec2(4.697624908, -4.697624908);
+const origin: vec2 = new Vec2(0.32, 264.18);
+const iscale: Vec2 = new Vec2(2, 2);
+
+const toKPos = function (pos: Vec2): Vec2 {
+    return new Vec2((pos.x * scale.x + origin.x)*iscale.x,(pos.y * scale.y + origin.y)*iscale.y);
+}
+
+const toPos = function (pos: Vec2): Vec2 {
+    return new Vec2((pos.x/iscale.x - origin.x) / scale.x, (pos.y/iscale.y - origin.y) / scale.y);
+}
+
+class ShovItem {
+    label: ELabel;
+    constructor(public doc: any, color: string) {
+        let pos = new Vec2(doc["xpos"], doc["ypos"]);
+        this.label = new ELabel(toKPos(pos), doc["_id"], color);
+        console.log(this.doc);
+    }
+
+    getKonvaObj() {
+        return this.label.label;
+    }
+
+    save(url) {
+        let kpos = this.label.label.position()
+        let pos = toPos(new Vec2(kpos.x, kpos.y));
+        $.ajax({
+            url: url, 
+            type: 'PUT',
+            data: JSON.stringify({"xpos": pos.x, "ypos": pos.y})
           });
     }
 }
 
+class ShovItemManager {
+    layer: any;
+    ShovItems: ShovItem[] = [];
+
+    constructor(public editor: Editor, public dburl: string, public color: string) {
+        this.layer = new Konva.Layer({});
+        editor.stage.add(this.layer);
+        $.get('http://omaraa.ddns.net:62027/db/all/' + dburl, (resp) => this.loadItems(resp));
+    }
+
+    loadItems(resp) {
+        let ids: string[] = resp;
+        for (let id of ids) {
+            $.get('http://omaraa.ddns.net:62027/db/' + this.dburl + '/' + id, (resp) => this.loadItem(resp));
+        }
+    }
+
+    loadItem(doc) {
+        let item = new ShovItem(doc, this.color);
+        this.ShovItems.push(item);
+        this.layer.add(item.getKonvaObj());
+        this.layer.draw();
+    }
+
+    save() {
+        for(let i of this.ShovItems) {
+            i.save('http://omaraa.ddns.net:62027/db/' + this.dburl + '/' + i.doc['_id']);
+        }
+    }
+}
+
+class Building {
+    layer: any;
+    imageObj: any;
+    floorplan: any;
+    constructor(public editor: Editor, public src: string) {
+        this.layer = new Konva.Layer({});
+        editor.stage.add(this.layer);
+
+        this.imageObj = new Image()
+        this.imageObj.onload = () => {
+            this.floorplan = new Konva.Image({
+                x: 0,
+                y: 0,
+                image: this.imageObj,
+                width: this.imageObj.width*iscale.x,
+                height: this.imageObj.height*iscale.y,
+                //draggable: true
+            });
+            this.layer.add(this.floorplan);
+            this.layer.draw();
+        };
+        this.imageObj.src = src
+    }
+
+
+}
+
 class Editor {
-    stage : any;
-    constructor(containerID : string) {
+    stage: any;
+    topbarElem: any;
+    beacons: ShovItemManager;
+    pies: ShovItemManger;
+    constructor(public container: any, public topbarelem: any) {
         this.stage = new Konva.Stage({
-            container: containerID,   // id of container <div>
-            width: $('body').innerWidth()-10,
-            height: $(window).innerHeight() - $('#headerbar').outerHeight(),
+            container: container, // id of container <div>
+            width: container.offsetWidth,
+            height: window.innerHeight - $('#topbar').height(),
             draggable: true,
             color: "white"
         });
+        console.log("Konva initialized!");
 
         let layer = new Konva.Layer({});
 
         layer.add(new Konva.Circle({
-            x: 20,
-            y: 20,
+            x: 0,
+            y: 0,
             fill: "red",
             radius: 10
         }));
 
         this.stage.add(layer);
+        this.bg = new Building(this, "http://omaraa.ddns.net:62027/db/buildings/eb2/L1.svg");
+        this.beacons = new ShovItemManager(this, "beacons", "black");
+        this.pies = new ShovItemManager(this, "pies", "red");
+
         this.stage.draw();
+
+        window.save = () => {this.save();};
+    }
+
+    save() {
+        this.beacons.save();
+        this.pies.save();
     }
 }
 
-let e = new Editor("container");
+$(document).ready(() => {
+    let container = document.getElementById('container');
+    let topbar = document.getElementById('topbar');
+    $("#topbar").ready(() => {
+        let e = new Editor(container, topbar);
+    })
+})
